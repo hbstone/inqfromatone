4cb164d853cc37b56e7bc864fc21b473784fb279
HB Stone <hbstone@users.noreply.github.com>
2024-12-28 10:49:39 -0800

    add CI with lint and prettifying
    * Create github-actions.yml and add lint
    
    * Update and rename github-actions.yml to lint.yml
    
    * Create package.json
    
    * Create package-lock.json
    
    * Update package-lock.json
    
    * Update package.json
    
    * Update package-lock.json
    
    * add eslint and prettier to ci
    
    * Create eslint.config
    
    * Add files via upload
    
    * lint
    
    * prettier
    
    * prettier
    
    * Delete eslint.config.mjs
    
    * Create eslint.config.js
    
    * Update lint.yml with custom action

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const net = require('net');
-const { handleSocket } = require('./modules/communication');
+import net from "net";
+import { handleSocket } from "./modules/communication";
 
 const server = net.createServer(handleSocket);
 
 server.listen(8484, () => {
-    console.log("MUD server is listening on port 8484");
+  console.log("MUD server is listening on port 8484");
 });
