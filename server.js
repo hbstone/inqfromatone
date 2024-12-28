7ae1b2cca508a350115017124bd3d2f8fb3b3202
HB Stone <hbstone@users.noreply.github.com>
2024-12-27 22:26:25 -0800

    prettier

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import net from 'net';
-import { handleSocket } from './modules/communication';
+import net from "net";
+import { handleSocket } from "./modules/communication";
 
 const server = net.createServer(handleSocket);
 
 server.listen(8484, () => {
-    console.log("MUD server is listening on port 8484");
+  console.log("MUD server is listening on port 8484");
 });
