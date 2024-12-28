0d27a3ee29a2147efca8678a5a07944dc47d8b86
HB Stone <hbstone@users.noreply.github.com>
2024-12-28 10:30:53 -0800

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    rules: {
+      "semi": "error",
+      "no-eval": "error",
+      "no-implied-eval": "error",
+      "prefer-promise-reject-errors": "warn",
+    }
+  }
+];
