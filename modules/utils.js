fd5a6c508c1270302698e35b99bba14351c58e2e
HB Stone <hbstone@users.noreply.github.com>
2024-12-29 18:50:26 +0000

    restore utils.js

---
+++ modules/utils.js
@@ -0,0 +1,14 @@
+function writeToSocket(socket, message) {
+  const formattedMessage = message.charAt(0).toUpperCase() + message.slice(1);
+  socket.write(formattedMessage + "\n");
+}
+
+function broadcast(room, message, excludeSocket = null) {
+  room.occupants.forEach((client) => {
+    if (client !== excludeSocket) {
+      writeToSocket(client, message);
+    }
+  });
+}
+
+export default { writeToSocket, broadcast };
