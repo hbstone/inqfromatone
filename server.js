05c09e149d87768b56a1ee5616b86b0538645354
HB Stone <hbstone@users.noreply.github.com>
2024-12-28 16:54:43 -0800

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-import net from "net";
-import { handleSocket } from "./modules/communication";
-
-const server = net.createServer(handleSocket);
-
-server.listen(8484, () => {
-  console.log("MUD server is listening on port 8484");
-});
