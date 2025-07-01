36b5eacfe69a9838a5ec385753033e251d3ff321
HB Stone <hbstone@users.noreply.github.com>
2025-07-01 10:55:51 -0700

    Configure ESLint for Node

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,11 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: { ...globals.node, ...globals.es2021 },
+    },
+  },
   pluginJs.configs.recommended,
   {
     files: ['*.js', '**/*.js'],
